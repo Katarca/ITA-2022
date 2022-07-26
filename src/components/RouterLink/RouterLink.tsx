d7f86dc1b1e894df068c0df985dd6c5beeab31ed
Katarína Soušková <souskovakatarina@gmail.com>
2022-07-26 21:39:22 +0200

    type definition fixed

--- src/components/RouterLink/RouterLink.tsx
+++ src/components/RouterLink/RouterLink.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 export interface LinkProps {
-  to: any
+  to: string
   target?: string
   children?: JSX.Element | JSX.Element[]
 }
