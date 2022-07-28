b001297137f42f058a43e769c050c174826f6a3b
Katarína Soušková <souskovakatarina@gmail.com>
2022-07-28 18:38:41 +0200

    globalstyle moved to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { CounterApp } from './homework2/CounterApp'
-import { GlobalStyle } from '.'
 import { HomePage } from './pages/HomePage'
 import { JavaScriptWeb } from './homework1/JavaScriptWeb'
 import { Route, Routes } from 'react-router-dom'
 import { StyledApp } from './page-style/StyledApp'
+import { colors } from './helpers/theme'
+import { createGlobalStyle } from 'styled-components'
 import React from 'react'
 
 export function App() {
@@ -18,3 +19,29 @@ export function App() {
     </StyledApp>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+    }
+
+    html {
+    font-size: 62.5%;
+    }
+
+    body {
+    box-sizing: border-box;
+    background-color: ${colors.blue900};
+    font-family: 'IBM Plex Mono', monospace;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+    body::-webkit-scrollbar {
+    display: none;
+  }
+
+`
--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,7 @@
 import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { colors } from './helpers/theme'
-import { createGlobalStyle } from 'styled-components'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-export const GlobalStyle = createGlobalStyle`
-    *,
-    *::before,
-    *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: inherit;
-    }
-
-    html {
-    font-size: 62.5%;
-    }
-
-    body {
-    box-sizing: border-box;
-    background-color: ${colors.blue900};
-    font-family: 'IBM Plex Mono', monospace;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-  }
-    body::-webkit-scrollbar {
-    display: none;
-  }
-
-`
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
