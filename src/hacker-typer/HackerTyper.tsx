f25e6cdb93028211035f399202d945316e7c93f9
Katarína Soušková <souskovakatarina@gmail.com>
2022-08-09 19:29:04 +0200

    hacker typer logic added

--- src/hacker-typer/HackerTyper.tsx
+++ src/hacker-typer/HackerTyper.tsx
@@ -1,17 +1,36 @@
 import { Div_Container } from '../components/Container'
+import { DummyCode } from './DummyCode'
 import { H_Heading } from '../components/Heading'
 import { P_LinkBodyText } from '../components/BodyText'
 import { RouterLink } from '../components/RouterLink'
 import { styles } from '../helpers/theme'
 import { urls } from '../helpers/urls'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
+const CODE = DummyCode
+const CODE_SEGMENT = 5
+
 export const HackerTyper = () => {
+  const [hackerCode, setHackerCode] = useState('')
+  const [index, setIndex] = useState(0)
+
+  const handleKeyDown = () => {
+    setIndex(index + CODE_SEGMENT)
+    setHackerCode(CODE.substring(0, index))
+    if (index === CODE.length) {
+      setIndex(0)
+    }
+  }
+
   return (
     <Div_Container>
       <H_HackHeading>Hacker Typer</H_HackHeading>
-      <TA_TextArea />
+      <TA_TextArea
+        value={!hackerCode ? 'Start typing...' : hackerCode}
+        onKeyDown={handleKeyDown}
+        autoFocus={true}
+      />
       <RouterLink to={urls.homePage}>
         <P_LinkBodyText>Return home</P_LinkBodyText>
       </RouterLink>
@@ -28,4 +47,12 @@ const TA_TextArea = styled.textarea`
   border: 2px solid ${styles.colors.orange300};
   border-radius: 8px;
   background: transparent;
+  font-size: ${styles.fontSize.xs};
+  color: ${styles.colors.orange300};
+  padding: ${styles.spacing.xs};
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
