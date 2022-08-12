d14d4228dee47df939522291009b2c9424dca883
Katarína Soušková <souskovakatarina@gmail.com>
2022-08-12 12:50:56 +0200

    useLocalStorage hook added to file hooks

--- src/todo-list/ToDoList.tsx
+++ src/todo-list/ToDoList.tsx
@@ -10,36 +10,9 @@ import { ToDo } from './ToDo'
 import { breakpoint, styles } from '../helpers/theme'
 import { idGenerator } from '../helpers/utils'
 import { urls } from '../helpers/urls'
+import { useLocalStorage } from '../helpers/hooks'
 import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
-
-export const useLocalStorage = <T,>(key: string, initialValue: T) => {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    if (typeof window === 'undefined') {
-      return initialValue
-    }
-    try {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      console.error()
-      return initialValue
-    }
-  })
-
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value
-      setStoredValue(valueToStore)
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore))
-      }
-    } catch (error) {
-      console.error()
-    }
-  }
-  return [storedValue, setValue] as const
-}
 
 type Props = {
   children: React.ReactNode
