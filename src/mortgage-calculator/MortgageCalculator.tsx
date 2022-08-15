bc2abb1c49e75aa3f31ca8d37b0f416ac372844c
Katarína Soušková <souskovakatarina@gmail.com>
2022-08-15 17:49:07 +0200

    inputs and monthly payment text added

--- src/mortgage-calculator/MortgageCalculator.tsx
+++ src/mortgage-calculator/MortgageCalculator.tsx
@@ -1,22 +1,28 @@
+import { CustomInput } from '../components/Input'
 import { Div_Container } from '../components/Container'
 import { H_Heading } from '../components/Heading'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { P_LinkBodyText } from '../components/BodyText'
+import { P_BodyText, P_LinkBodyText } from '../components/BodyText'
 import { RouterLink } from '../components/RouterLink'
 import { styles } from '../helpers/theme'
 import { urls } from '../helpers/urls'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const paymentCalc = (loan: number, rate: number, months: number) => {
   const monthlyRate = rate / 100 / 12
-  return Math.ceil(
+  return (
     (loan * monthlyRate * Math.pow(1 + monthlyRate, months)) /
-      (Math.pow(1 + monthlyRate, months) - 1)
+    (Math.pow(1 + monthlyRate, months) - 1)
   )
 }
 
 export const MortgageCalculator = () => {
+  const [loanAmount, setLoanAmount] = useState(0)
+  const [interestRate, setInterestRate] = useState(0)
+  const [numMonths, setNumMonths] = useState(0)
+  const monthlyPayment = paymentCalc(loanAmount, interestRate, numMonths)
+
   return (
     <HelmetProvider>
       <Helmet>
@@ -24,6 +30,27 @@ export const MortgageCalculator = () => {
       </Helmet>
       <Div_Container>
         <H_MCHeading>Mortgage Calculator</H_MCHeading>
+        <div>
+          <CustomInput
+            type='number'
+            placeholder='Loan Amount'
+            onChange={e => setLoanAmount(Number(e.target.value))}
+          />
+          <CustomInput
+            type='number'
+            placeholder='Interest Rate'
+            onChange={e => setInterestRate(Number(e.target.value))}
+          />
+          <CustomInput
+            type='number'
+            placeholder='Number of Months'
+            onChange={e => setNumMonths(Number(e.target.value))}
+          />
+          <P_BodyText>
+            Estimated monthly payments{' '}
+            {!monthlyPayment || !isFinite(monthlyPayment) ? '0.00' : monthlyPayment.toFixed(2)} CZK
+          </P_BodyText>
+        </div>
         <RouterLink to={urls.homePage}>
           <P_LinkBodyText>Return home</P_LinkBodyText>
         </RouterLink>
