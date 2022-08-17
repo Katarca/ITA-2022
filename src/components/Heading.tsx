a9e0d0d5992f31e34379e731fb0e17cc7de86f3b
Katarína Soušková <souskovakatarina@gmail.com>
2022-08-17 12:28:11 +0200

    mortgage calculator refactored

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,6 +2,7 @@ import { styles } from '../helpers/theme'
 import styled from 'styled-components'
 
 export const H_Heading = styled.h1`
+  text-align: center;
   font-size: ${styles.fontSize.xxl};
   padding: ${styles.spacing.sm};
   color: ${styles.colors.orange300};
