f7ed092cd4dbcb34124f69f94e5b2bfecef1bdbe
Katarína Soušková <souskovakatarina@gmail.com>
2022-08-18 17:19:26 +0200

    users data and get request added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,18 @@
-console.log('hello')
+import cors from 'cors'
+import express from 'express'
+import fs from 'fs'
 
-export {}
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  const dataString = fs.readFileSync(`${__dirname}/../data.json`, 'utf-8')
+  const data = JSON.parse(dataString).users
+  res.send(data)
+})
+
+app.listen(port, () => {
+  console.info(`App listening on port ${port}`)
+})
