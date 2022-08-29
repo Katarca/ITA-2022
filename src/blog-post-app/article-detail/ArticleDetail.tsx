49e71a75d43e4787c5d73d0e183fb4b8b9d0a0f9
Katarína Soušková <souskovakatarina@gmail.com>
2022-08-29 17:43:19 +0200

    editing feature added

--- src/blog-post-app/article-detail/ArticleDetail.tsx
+++ src/blog-post-app/article-detail/ArticleDetail.tsx
@@ -1,56 +1,26 @@
 import { ArticleDetailStateContext } from './ArticleDetailContext'
-import { Div_MsgContainer, P_BlogText } from '../articles/Articles'
+import { CustomInput } from '../../components/Input'
+import { CustomTextarea } from '../../components/Textarea'
+import { Div_MsgContainer, P_BlogText, P_BlogTextXs } from '../articles/Articles'
+import { Form } from '../../components/Form'
 import { H_Heading, H_SubHeading } from '../../components/Heading'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { MarkDown } from '../../components/MarkDown'
 import { P_BodyText, P_LinkBodyText } from '../../components/BodyText'
 import { RouterLink } from '../../components/RouterLink'
 import { TransparentButtonBorder } from '../../components/Button'
-import { blogAppUrl, urls } from '../../helpers/urls'
+import { blogAppUrl, urlString, urls } from '../../helpers/urls'
 import { breakpoint, styles } from '../../helpers/theme'
+import { getSlug } from '../../utils/getSlug'
 import { useNavigate, useParams } from 'react-router-dom'
 import React, { useContext, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 export const ArticleDetail = () => {
-  const { id } = useParams<{ id: string }>()
-
   const articleDetailLogic = useContext(ArticleDetailStateContext)
-  const [loading, setLoading] = useState(false)
-  const [errorMsg, setErrorMsg] = useState('')
+  const [editing, setEditing] = useState(false)
 
   let navigate = useNavigate()
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      setLoading(true)
-      try {
-        const response = await fetch(`${blogAppUrl}${id}`)
-        if (!response.ok) throw Error
-        const json = await response.json()
-        articleDetailLogic.setArticleDetail(json)
-      } catch (error) {
-        console.error(error)
-        setErrorMsg(`An error occurred while fetching article`)
-      }
-      setLoading(false)
-    }
-    fetchArticles()
-  }, [])
-
-  const deleteArticle = async () => {
-    try {
-      const response = await fetch(`${blogAppUrl}${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      if (!response.ok) throw Error
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
   const loadingJSX = (
     <Div_MsgContainer>
@@ -60,7 +30,7 @@ export const ArticleDetail = () => {
 
   const errorJSX = (
     <Div_MsgContainer>
-      <P_BlogText>{errorMsg}</P_BlogText>
+      <P_BlogText>{articleDetailLogic.errorMsg}</P_BlogText>
       <RouterLink to={urls.blogApp}>
         <P_LinkBodyText>Return to articles</P_LinkBodyText>
       </RouterLink>
@@ -69,27 +39,85 @@ export const ArticleDetail = () => {
 
   const articleDetailJSX = (
     <>
-      <H_ArticleHeading>{articleDetailLogic.articleDetail.title}</H_ArticleHeading>
-      <Div_DetailContainer>
-        <P_BlogText>Author: {articleDetailLogic.articleDetail.author}</P_BlogText>
-        <P_BlogText>created at {articleDetailLogic.articleDetail.date}</P_BlogText>
-      </Div_DetailContainer>
-      <Div_ContentContainer>
-        <MarkDown>{articleDetailLogic.articleDetail.content}</MarkDown>
-      </Div_ContentContainer>
-      <Div_ButtonContainer>
-        <TransparentButtonBorder>
-          <P_BodyText>Edit</P_BodyText>
-        </TransparentButtonBorder>
-        <TransparentButtonBorder
-          onClick={() => {
-            deleteArticle()
+      {editing ? (
+        <Form
+          onSubmit={e => {
+            e.preventDefault()
+            if (!articleDetailLogic.validateInputs()) return
+            articleDetailLogic.postData()
+            setEditing(false)
             navigate(urls.blogApp)
           }}
         >
-          <P_BodyText>Delete</P_BodyText>
-        </TransparentButtonBorder>
-      </Div_ButtonContainer>
+          <Div_InputContainer>
+            <Label_EditLabel>Title</Label_EditLabel>
+            <CustomInput
+              type='text'
+              value={articleDetailLogic.newTitle}
+              onChange={e => articleDetailLogic.setNewTitle(e.target.value)}
+            />
+            {articleDetailLogic.newTitleErr && (
+              <Div_ErrContainer>
+                <P_BlogTextXs>{articleDetailLogic.newTitleErr}</P_BlogTextXs>
+              </Div_ErrContainer>
+            )}
+          </Div_InputContainer>
+          <Div_InputContainer>
+            <Label_EditLabel>Author</Label_EditLabel>
+            <CustomInput
+              type='text'
+              value={articleDetailLogic.newAuthor}
+              onChange={e => articleDetailLogic.setNewAuthor(e.target.value)}
+            />
+            {articleDetailLogic.newAuthorErr && (
+              <Div_ErrContainer>
+                <P_BlogTextXs>{articleDetailLogic.newAuthorErr}</P_BlogTextXs>
+              </Div_ErrContainer>
+            )}
+          </Div_InputContainer>
+          <Div_InputContainer>
+            <Label_EditLabel>Content</Label_EditLabel>
+            <EditTextarea
+              value={articleDetailLogic.newContent}
+              onChange={e => articleDetailLogic.setNewContent(e.target.value)}
+            />
+            {articleDetailLogic.newContentErr && (
+              <Div_ErrContainer>
+                <P_BlogTextXs>{articleDetailLogic.newContentErr}</P_BlogTextXs>
+              </Div_ErrContainer>
+            )}
+          </Div_InputContainer>
+          <Div_ButtonContainer>
+            <TransparentButtonBorder type='submit'>
+              <P_BodyText>Submit</P_BodyText>
+            </TransparentButtonBorder>
+          </Div_ButtonContainer>
+        </Form>
+      ) : (
+        <>
+          <H_ArticleHeading>{articleDetailLogic.articleDetail.title}</H_ArticleHeading>
+          <Div_DetailContainer>
+            <P_BlogText>Author: {articleDetailLogic.articleDetail.author}</P_BlogText>
+            <P_BlogText>created at {articleDetailLogic.articleDetail.date}</P_BlogText>
+          </Div_DetailContainer>
+          <Div_ContentContainer>
+            <MarkDown>{articleDetailLogic.articleDetail.content}</MarkDown>
+          </Div_ContentContainer>
+          <Div_ButtonContainer>
+            <TransparentButtonBorder onClick={() => setEditing(true)}>
+              <P_BodyText>Edit</P_BodyText>
+            </TransparentButtonBorder>
+            <TransparentButtonBorder
+              onClick={() => {
+                articleDetailLogic.deleteArticle()
+                navigate(urls.blogApp)
+              }}
+            >
+              <P_BodyText>Delete</P_BodyText>
+            </TransparentButtonBorder>
+          </Div_ButtonContainer>
+        </>
+      )}
     </>
   )
 
@@ -99,7 +127,11 @@ export const ArticleDetail = () => {
         <title>{articleDetailLogic?.articleDetail.title}</title>
       </Helmet>
       <Div_ArticleContainer>
-        {loading ? loadingJSX : errorMsg ? errorJSX : articleDetailJSX}
+        {articleDetailLogic.loading
+          ? loadingJSX
+          : articleDetailLogic.errorMsg
+          ? errorJSX
+          : articleDetailJSX}
       </Div_ArticleContainer>
     </HelmetProvider>
   )
@@ -137,4 +169,27 @@ const Div_ContentContainer = styled.div`
 const Div_ButtonContainer = styled.div`
   display: flex;
   padding: ${styles.spacing.sm} 0;
+`
+const Div_InputContainer = styled.div`
+  padding: 0 0 ${styles.spacing.sm} 0;
+  display: flex;
+  flex-direction: column;
+`
+
+const Div_ErrContainer = styled.div`
+  padding: 0 ${styles.spacing.xs};
+`
+
+const Label_EditLabel = styled.label`
+  font-size: ${styles.fontSize.sm};
+  color: ${styles.colors.grey300};
+  padding: 0 ${styles.spacing.xs};
+`
+
+const EditTextarea = styled(CustomTextarea)`
+  font-size: ${styles.fontSize.sm};
+  color: ${styles.colors.white};
+  padding: ${styles.spacing.xs} ${styles.spacing.sm};
+  margin: ${styles.spacing.xs};
+  height: 400px;
 `
--- src/blog-post-app/article-detail/ArticleDetailContext.tsx
+++ src/blog-post-app/article-detail/ArticleDetailContext.tsx
@@ -1,13 +1,109 @@
 import { Article } from '../articles/ArticlesContext'
 import { ArticleDetail } from './ArticleDetail'
+import { blogAppUrl } from '../../helpers/urls'
 import { genericHookContextBuilder } from '../../utils/genericHookContextBuilder'
-import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 
 const useLogicState = () => {
+  const { id } = useParams<{ id: string }>()
   const [articleDetail, setArticleDetail] = useState({} as Article)
+  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+  const [newTitleErr, setNewTitleErr] = useState('')
+  const [newAuthorErr, setNewAuthorErr] = useState('')
+  const [newContentErr, setNewContentErr] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newContent, setNewContent] = useState('')
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch(`${blogAppUrl}${id}`)
+        if (!response.ok) throw Error
+        const json = await response.json()
+        setArticleDetail(json)
+        setNewTitle(json.title)
+        setNewAuthor(json.author)
+        setNewContent(json.content)
+      } catch (error) {
+        console.error(error)
+        setErrorMsg(`An error occurred while fetching article`)
+      }
+      setLoading(false)
+    }
+    fetchArticles()
+  }, [])
+
+  const deleteArticle = async () => {
+    try {
+      const response = await fetch(`${blogAppUrl}${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (!response.ok) throw Error
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const validateInputs = () => {
+    let validInputs = true
+    if (newTitle.trim().length === 0) {
+      validInputs = false
+      setNewTitleErr('Title is required')
+    }
+    if (newAuthor.trim().length === 0) {
+      validInputs = false
+      setNewAuthorErr('Author is required')
+    }
+    if (newContent.trim().length === 0) {
+      validInputs = false
+      setNewContentErr('Content is required')
+    }
+    return validInputs
+  }
+
+  const postData = async () => {
+    try {
+      const response = await fetch(`${blogAppUrl}${id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: newTitle,
+          author: newAuthor,
+          content: newContent,
+        }),
+      })
+      if (!response.ok) throw Error
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return {
     articleDetail,
     setArticleDetail,
+    loading,
+    errorMsg,
+    deleteArticle,
+    validateInputs,
+    postData,
+    newTitleErr,
+    newAuthorErr,
+    newContentErr,
+    newTitle,
+    newAuthor,
+    newContent,
+    setNewTitle,
+    setNewAuthor,
+    setNewContent,
   }
 }
 
