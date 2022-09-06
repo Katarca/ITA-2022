ff7a40dd9f31c026fa4b2ecc97ffc1fb8dd27749
Katarína Soušková <souskovakatarina@gmail.com>
2022-09-06 20:48:45 +0200

    project cards added

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,36 +1,66 @@
-import { P_LinkBodyText } from './components/BodyText'
+import { Card } from './components/Card'
+import { Link } from 'react-router-dom'
+import { P_BodyText, P_LinkBodyText } from './components/BodyText'
 import { RouterLink } from './components/RouterLink'
+import { ReactComponent as githubLogo } from './images/github.svg'
 import { styles } from './helpers/theme'
 import { urls } from './helpers/urls'
 import React from 'react'
+import blogImg from './images/blog.png'
+import blogPostImg from './images/blogpostapp.png'
+import hackerTyperImg from './images/hackertyper.png'
+import jsImg from './images/jsweb.png'
+import memoryGameImg from './images/memorygame.png'
 import styled from 'styled-components'
+import todoImg from './images/todolist.png'
 
 export const Projects = () => {
   return (
     <Div_ProjectsSection>
-      <RouterLink to={urls.jsWeb}>
-        <P_LinkBodyText>Javascript Web</P_LinkBodyText>
-      </RouterLink>
-      <RouterLink to={urls.todoList}>
-        <P_LinkBodyText>ToDo List</P_LinkBodyText>
-      </RouterLink>
-      <RouterLink to={urls.hackerTyper}>
-        <P_LinkBodyText>Hacker Typer</P_LinkBodyText>
-      </RouterLink>
-      <RouterLink to={urls.memoryGame}>
-        <P_LinkBodyText>Memory Game</P_LinkBodyText>
-      </RouterLink>
+      <Card
+        text='ToDo List'
+        to={urls.todoList}
+        src={todoImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/todo-list'
+      />
+      <Card
+        text='ToDo List Redux'
+        to={urls.todoListRedux}
+        src={todoImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/todo-list-redux'
+      />
+      <Card
+        text='JavaScript Web'
+        to={urls.jsWeb}
+        src={jsImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/javascript-web'
+      />
+      <Card
+        text='Hacker Typer'
+        to={urls.hackerTyper}
+        src={hackerTyperImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/hacker-typer'
+      />
+      <Card
+        text='Memory Game'
+        to={urls.memoryGame}
+        src={memoryGameImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/memory-game'
+      />
+      <Card
+        text='Blog'
+        to={urls.blog}
+        src={blogImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/blog'
+      />
+      <Card
+        text='Blog Post App'
+        to={urls.blogApp}
+        src={blogPostImg}
+        githubUrl='https://github.com/Katarca/ITA-2022/tree/main/src/blog-post-app'
+      />
       <RouterLink to={urls.mortgageCalculator}>
         <P_LinkBodyText>Mortgage Calculator</P_LinkBodyText>
-      </RouterLink>
-      <RouterLink to={urls.blog}>
-        <P_LinkBodyText>Blog</P_LinkBodyText>
-      </RouterLink>
-      <RouterLink to={urls.blogApp}>
-        <P_LinkBodyText>Blog Post App</P_LinkBodyText>
-      </RouterLink>
-      <RouterLink to={urls.todoListRedux}>
-        <P_LinkBodyText>ToDo List Redux</P_LinkBodyText>
       </RouterLink>
     </Div_ProjectsSection>
   )
---
+++ src/components/Card.tsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom'
+import { P_BodyText } from './BodyText'
+import { ReactComponent as githubLogo } from '../images/github.svg'
+import { styles } from '../helpers/theme'
+import React from 'react'
+import styled from 'styled-components'
+
+type CardProps = {
+  text: string
+  to: string
+  src: string
+  githubUrl: string
+}
+
+export const Card = (props: CardProps) => {
+  return (
+    <Div_ProjectCard>
+      <Div_CardHeader>
+        <P_CardText>{props.text}</P_CardText>
+        <a href={props.githubUrl} target='_blank' rel='noreferrer'>
+          <GithubLogo />
+        </a>
+      </Div_CardHeader>
+      <Link_CardLink to={props.to}>
+        <Img_ProjectImg src={props.src} />
+      </Link_CardLink>
+    </Div_ProjectCard>
+  )
+}
+
+const Div_ProjectCard = styled.div`
+  width: 350px;
+  border-radius: 8px;
+  border: 1px solid ${styles.colors.orangeTransparent};
+`
+
+const Img_ProjectImg = styled.img`
+  width: 100%;
+  border-radius: 8px;
+  filter: grayscale(60%);
+  opacity: 0.4;
+`
+
+const Link_CardLink = styled(Link)`
+  text-decoration: none;
+`
+
+const P_CardText = styled(P_BodyText)`
+  font-size: ${styles.fontSize.sm};
+  text-align: center;
+`
+const Div_CardHeader = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+const GithubLogo = styled(githubLogo)`
+  width: 40px;
+`
